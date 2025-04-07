03e1f2f2bdb354be2e907eed04f51489d4fbd1e2
Coleman Broaddus <coleman.broaddus@gmail.com>
2025-04-07 14:08:23 -0400

    Tidy power of two analysis with new sqlpeek.

--- p2.sql
+++ p2.sql
@@ -1,21 +1,73 @@
 -- .header on
 .mode col
 
--- sqlpeek
--- x=time y=reward c=value row=decay
-select * from wire_pow_of_two
+
+-- First, list all the tables.
+select name, type FROM sqlite_master
 ;
 
--- Interesting how you don't see a massive difference
--- in the plots or in the total count of unique powers
--- of two.
+-- Quick snapshot of the main table and columns.
+select *, count() from wire_pow_of_two
+group by campaign_id
+;
 
--- sqlpeek 
-select distinct campaign_id, proglen, decay
+-- Table of Campaigns.
+create view if not exists Camp as
+select distinct decay, proglen, campaign_id from wire_pow_of_two ;
+
+-- Make a table of all the unique powers of two for each campaign.
+-- create view UniquePo2 as
+with
+A as ( select distinct campaign_id, log(2, value) lg2val  from wire_pow_of_two ),
+B as ( select *, floor(lg2val)=lg2val isPo2 from A ),
+C as ( select * from B where isPo2 = true),
+D as ( select C.*, P.decay, P.proglen from C join Camp P using (campaign_id))
+select * from D -- unique powers of two for each campaign
+;
+
+-- Count unique powers of two per campaign For i64 values there are
+-- only 63 possible unique values! If we get 54 unique values in a
+-- single campaign, that's pretty good?
+select campaign_id, decay, proglen, count() total_unique
+from UniquePo2
+group by campaign_id 
+;
+
+-- Which specific powers of two are uniquely present in which campaign?
+with
+A as (
+select *, count() over (partition by lg2val) as cnt
+from UniquePo2
+)
+select * from A
+where cnt = 2
+;
+
+-- Let's look at the reward over time. Does it look like the fancy
+-- wiring approaches find powers of two earlier and then peter out?
+-- x=time y=logrew c=logrew row=decay
+select *, log(2, reward) as logrew
 from wire_pow_of_two
+where reward > 0
 ;
 
--- sqlpeek
+
+-- Plot cumulative log-reward over time
+-- x=time y=sum_reward c=decay
+with
+A as (
+select *, log(2, reward) as logrew
+from wire_pow_of_two
+where reward > 0
+)
+select *, sum(-logrew) over (partition by campaign_id order by time) "sum_reward"
+from A
+;
+
+-- Reward Cumulative Distribution
+-- Interesting how you don't see a massive difference in the plots or
+-- in the total count of unique powers of two. Try toggling `row` to
+-- see how the reward distributions perfectly overlap.
 -- x=logrew y=bucket row=campaign_id c=campaign_id
 with
 S as (
@@ -32,32 +84,18 @@ B as (
     from A
     group by bucket
 )
-select * from B
+    select * from B
 ;
 
--- sqlpeek
-SELECT name FROM sqlite_master WHERE type='table';
 
--- sqlpeek
-create view campaignparams as
-select distinct decay, proglen, campaign_id from wire_pow_of_two ;
 
--- unique powers-of-two by campaign
--- sqlpeek  
-with
-A as ( select distinct campaign_id, log(2, value) lg2val  from wire_pow_of_two ),
-B as ( select *, floor(lg2val)=lg2val isPo2 from A ),
-C as ( select * from B where isPo2 = true),
-campaignparams as (select distinct decay, proglen, campaign_id from wire_pow_of_two),
-D as ( select C.*, P.decay, P.proglen from C join campaignparams P using (campaign_id)),
-E as ( select campaign_id, decay, proglen, count() total_unique from D group by campaign_id )
--- select * from D -- unique powers of two for each campaign
-select * from E -- count of D by campaign
 
--- where isPo2 = 1
--- group by campaign_id
--- order by cnt
-;
+
+
+
+
+
+
 
 
 -- select only rows from campaigns with more than 1k rows
