4a10b44795a2b0d536a2c5483feb7ebd1f84cc59
Coleman Broaddus <coleman.broaddus@gmail.com>
2025-04-18 16:36:40 -0400

    Snapshot Experiment #zero-absorb

--- prog_gen_lvl2.go
+++ prog_gen_lvl2.go
@@ -13,28 +13,18 @@ func cloneLib(lib Library) Library {
 
 func sample2lvl() Program {
 	value_library = make(map[Sym]Value)
-	// var p Program
-	// prog := Program{}
 	sp := newSampleParams()
 
 	lib := NewLib()
 	lib.addBasicMathLib()
 	sp.Program_length = 3
 	p0 := lib.sampleProgram(sp)
-	// prog = append(prog, p...)
 
-	sp.Program_length = 10
+	sp.Program_length = 50
 	lib2 := cloneLib(lib)
 	delete(lib2.fns, "one")
 	sp.Prefix = p0
-	// fmt.Printf("lib %+v", lib2)
+	// fmt.Printf("lib2 %+v", lib2)
 	p1 := lib2.sampleProgram(sp)
-	// prog = p
-	// printProgram(p0, Fmt)
-	// printProgram(p1, Fmt)
-	// fmt.Println(p0)
-	// fmt.Println(p1)
-	// p0 = append(p0, p1[len(p0):]...)
-
 	return p1
 }
